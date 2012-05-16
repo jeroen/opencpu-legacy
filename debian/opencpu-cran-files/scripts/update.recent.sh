ee03e01aff79516fa46d91ed84d13ac781528b14
Jeroen <jeroen@jeroen-ubuntu.(none)>
2012-05-16 16:13:05 -0700

    opencpu 0.7 fix

--- debian/opencpu-cran-files/scripts/update.recent.sh
+++ debian/opencpu-cran-files/scripts/update.recent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/log/opencpu
 apt-get update
-apt-get -y install ^r-cran* ^r-bioc*
+apt-get -y install ^r-cran* #^r-bioc*
 apt-get clean
 
 sudo -u opencpu R -e "library(opencpu.tools, lib.loc='/usr/lib/opencpu/opencpu-system-library'); install.recent.packages(libpath='/mnt/export/opencpu-cran-library');"
